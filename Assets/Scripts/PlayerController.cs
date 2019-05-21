2bc43c29f106debcf0ea462c96c3e8823c46eeb1
Guilherme Galarça Dutra <udutra@gmail.com>
2019-05-21 11:47:20 -0300

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float velocidade;
+
+    void FixedUpdate()
+    {
+        transform.position = Vector2.MoveTowards(transform.position,GetComponentInChildren<Camera>().ScreenToWorldPoint(Input.mousePosition), velocidade * Time.deltaTime);    
+    }
+}
